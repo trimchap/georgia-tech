4a02c51ca6b11870f1b1e23b7ef9e3ddc8614611
TrimChap <trimchap@trishtunney.com>
2020-08-11 10:19:03 -0700

    binary search

--- searching-algorithms.py
+++ searching-algorithms.py
@@ -18,3 +18,52 @@ def linear(lst, element):
 a_list = [5, 1, 3, 6, 7, 3, 1, 6, 7, 8, 3, 6]
 print("LINEAR SEARCH: ")
 print(linear(a_list, 6))
+
+#----------------------------------------------------------------------
+# BINARY SEARCH   - O(logn)
+#
+# Takes a list and a search term and returns True if the term is found 
+# in the list and False if not
+#----------------------------------------------------------------------
+def binary_search(searchList, searchTerm):
+
+    # the list must be sorted
+    searchList.sort()
+    
+    min = 0
+    max = len(searchList) - 1
+    
+    # loop through while there is 1 term or more in the list
+    while min <= max:
+        print("Searching for", searchTerm, "in", searchList[min:max+1])
+        
+        # Find the middle - use integer math
+        currentMiddle = (min + max) // 2
+
+        if searchList[currentMiddle] == searchTerm:
+            return True             # term found
+        elif searchTerm < searchList[currentMiddle]:
+            max = currentMiddle - 1 # term is less than middle, throw away right half
+        else:
+            min = currentMiddle + 1 # term is greater than middle, throw away left half
+
+    # term not found in list
+    return False
+
+# Test BINARY SEARCH
+intlist = [12, 64, 23, 3, 57, 19, 1, 17, 51, 62]
+print("23 is in list:", binary_search(intlist, 23))
+print("--")
+print("50 is in list:", binary_search(intlist, 50))
+print("--")
+strlist = ["David", "Joshua", "Marguerite", "Jackie"]
+print("David is in list:", binary_search(strlist, "David"))
+print("--")
+print("Lucy is in list:", binary_search(strlist, "Lucy"))
+print("--")
+from datetime import date
+datelist = [date(1885, 10, 13), date(2014, 11, 29), date(2016, 11, 26)]
+print("10/13/1885 is in list:", binary_search(datelist, date(1885, 10, 13)))
+print("--")
+print("11/28/2015 is in list:", binary_search(datelist, date(2015, 11, 28)))
+print("--")
